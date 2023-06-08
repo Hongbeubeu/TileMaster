ac977230c84e879558682bb2704374fc9e09bb86
Hongbeubeu <44913755+Hongbeubeu@users.noreply.github.com>
2023-06-09 00:09:18 +0700

    Update sprite demo

---
+++ Assets/_Project/Scripts/Datas/TemplateData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TemplateData", menuName = "Data/TemplateData", order = 0)]
+public class TemplateData : ScriptableObject
+{
+    public List<LayerBoardData> LayerBoardDatas;
+    public float LayerSpacing;
+    public int NumberOfLayer => LayerBoardDatas.Count;
+}
+
+[Serializable]
+public class LayerBoardData
+{
+    public int LayerIndex;
+    public List<Vector2> TilePositions;
+}
--- Assets/_Project/Scripts/Demo.cs
+++ Assets/_Project/Scripts/Demo.cs
@@ -1,18 +1,43 @@
-using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEditor;
 using UnityEngine;
 
 public class Demo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TemplateData _templateData;
+    [SerializeField] private GameObject _tilePrefab;
+
+    [SerializeField, ReadOnly] private List<GameObject> _tiles = new();
+
+    [Button(ButtonSizes.Gigantic)]
+    private void GenerateBoard()
     {
-        
+        DestroyInstancedTiles();
+        var position = Vector3.zero;
+        position.z = -10f;
+        for (var i = 0; i < _templateData.NumberOfLayer; i++)
+        {
+            for (var j = 0; j < _templateData.LayerBoardDatas[i].TilePositions.Count; j++)
+            {
+                var go = PrefabUtility.InstantiatePrefab(_tilePrefab, transform) as GameObject;
+                position.x = _templateData.LayerBoardDatas[i].TilePositions[j].x;
+                position.y = _templateData.LayerBoardDatas[i].TilePositions[j].y;
+                go.transform.localPosition = position;
+                _tiles.Add(go);
+            }
+
+            position.z += _templateData.LayerSpacing;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void DestroyInstancedTiles()
     {
-        
+        foreach (var t in _tiles)
+        {
+            DestroyImmediate(t);
+        }
+
+        _tiles.Clear();
     }
 }
